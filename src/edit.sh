39e290a21f62bc8781deb6ab82fb900779713dd1
Gabrielle Peregrin <gabrielp@si-d5.kzn.21-school.ru>
2023-03-07 15:50:05 +0300

    added edit.sh

---
+++ src/edit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#########################
+echo "Введите какую строку хотите заменить"
+read strin
+echo "Введите на что хотите заменить"
+read strout
+echo "Введите путь до файла где хотите заменить"
+read file
+
+sed -i "" "s/$strin/$strout/" $file
+
+################################
+info=$(ls -lh $file)
+#echo $info
+size=$(echo $info | cut -d ' ' -f5)
+#echo $size
+year=$(date "+%Y")
+month=$(echo $info | cut -d ' ' -f6)
+#echo $month
+day=$(echo $info | cut -d ' ' -f7)
+#echo $day
+time=$(echo $info | cut -d ' ' -f8)
+hash=$(shasum -a 256 $file)
+hash=$(echo $hash | cut -d ' ' -f1)
+alg="sha256"
+
+totalstr="$file - $size - $year-$month-$day - $time - $hash - $alg"
+echo $totalstr
+echo $totalstr >> files.log
+
+######################## 
+
+
